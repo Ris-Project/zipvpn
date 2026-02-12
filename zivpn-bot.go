f8944bda4e0b9191339a433f83742f20bcfedc68
Ris-Project <riswanxproject@gmail.com>
2026-02-12 21:57:49 +0700

    Add files via upload

--- zivpn-bot.go
+++ zivpn-bot.go
@@ -24,7 +24,7 @@ const (
     ApiUrl        = "http://127.0.0.1:8080/api"
     ApiKeyFile    = "/etc/zivpn/apikey"
     // !!! GANTI INI DENGAN URL GAMBAR MENU ANDA !!!
-    MenuPhotoURL = "https://h.uguu.se/LfWhbfvw.png"
+    MenuPhotoURL = "https://o.uguu.se/WYSyBMyU.png"
 
     // Interval untuk pengecekan dan penghapusan akun expired
     AutoDeleteInterval = 30 * time.Second
